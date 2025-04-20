5be66f6bd11ce42be186bbede1a6c169dd243450
hantianzhun <hl14332232020@outlook.com>
2025-04-20 17:29:08 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -39,7 +39,7 @@ export default defineUserConfig({
         // 仓库所有者的用户名
         user: "hantianzhun",
         // 仓库所有者的仓库名
-        repository: "Flist",
+        repository: "FList",
         // github 授权 Token, process.env.xxx  xxx 是环境变量名称。可以通过设置 githubToken 环境变量来配置
         authorizationToken: process.env.githubToken,
         // 分页大小，不懂得话就当作取最新的多少个标签吧。
